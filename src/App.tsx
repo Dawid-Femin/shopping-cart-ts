0d6fb3e84a3b1c29fb50e027050dba4402f14f3a
Dawid Femin <dawid.femin@gmail.com>
2023-03-23 23:21:14 +0100

    Created routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Store from './pages/Store/Store'
+import About from './pages/About/About'
+
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/store' element={<Store />}/>
+      <Route path='/about' element={<About />}/>
+    </Routes>
+    </>
   )
 }
 
